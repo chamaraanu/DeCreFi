6501c00dd9fbba8dc800fa90db95a538128bb8d3
Chamara Ariyarathne <chamaraanu@users.noreply.github.com>
2024-11-20 15:59:22 +1100

    Initial hardhat project structure

---
+++ blockchain/hardhat.config.ts
@@ -0,0 +1,30 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+const DEPLOY_PRIVATE_KEY = process.env.DEPLOY_PRIVATE_KEY != undefined ? process.env.DEPLOY_PRIVATE_KEY:""
+const INFURA_SEPOLIA_API_KEY = process.env.INFURA_SEPOLIA_API_KEY != undefined ? process.env.INFURA_SEPOLIA_API_KEY:""
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.7",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks:{
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_SEPOLIA_API_KEY}`,
+      accounts: [DEPLOY_PRIVATE_KEY],
+    },
+  }
+};
+
+export default config;
