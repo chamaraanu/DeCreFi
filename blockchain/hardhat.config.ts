f12b97ceb1e89c4ac075179d6d8d8c63e9c3866e
Chamara Ariyarathne <chamaraanu@users.noreply.github.com>
2024-11-27 10:36:03 +1100

    sepolia tests

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -6,7 +6,10 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 dotenv.config();
-const DEPLOY_PRIVATE_KEY = process.env.DEPLOY_PRIVATE_KEY != undefined ? process.env.DEPLOY_PRIVATE_KEY:""
+const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY != undefined ? process.env.DEPLOYER_PRIVATE_KEY:""
+const INVESTOR_PRIVATE_KEY = process.env.INVESTOR_PRIVATE_KEY != undefined ? process.env.INVESTOR_PRIVATE_KEY:""
+const ORIGINATOR_PRIVATE_KEY = process.env.ORIGINATOR_PRIVATE_KEY != undefined ? process.env.ORIGINATOR_PRIVATE_KEY:""
+const BORROWER_PRIVATE_KEY = process.env.BORROWER_PRIVATE_KEY != undefined ? process.env.BORROWER_PRIVATE_KEY:""
 const INFURA_SEPOLIA_API_KEY = process.env.INFURA_SEPOLIA_API_KEY != undefined ? process.env.INFURA_SEPOLIA_API_KEY:""
 
 const config: HardhatUserConfig = {
@@ -25,7 +28,7 @@ const config: HardhatUserConfig = {
     },
     sepolia: {
       url: `https://sepolia.infura.io/v3/${INFURA_SEPOLIA_API_KEY}`,
-      accounts: [DEPLOY_PRIVATE_KEY],
+      accounts: [DEPLOYER_PRIVATE_KEY, INVESTOR_PRIVATE_KEY, ORIGINATOR_PRIVATE_KEY, BORROWER_PRIVATE_KEY],
     },
   }
 };
