5202440828444f29f0c0747ed29f8f90e25cb43e
Chamara Ariyarathne <chamaraanu@users.noreply.github.com>
2024-11-29 15:35:51 +1100

    Initialize subgraph

---
+++ graph/decrefi4/src/vault.ts
@@ -0,0 +1,288 @@
+import {
+  Approval as ApprovalEvent,
+  Deposit as DepositEvent,
+  Initialized as InitializedEvent,
+  InvestorAdded as InvestorAddedEvent,
+  InvestorDeposited as InvestorDepositedEvent,
+  InvestorRedeemed as InvestorRedeemedEvent,
+  InvestorWithdrawn as InvestorWithdrawnEvent,
+  LoanFunded as LoanFundedEvent,
+  LoanRepaid as LoanRepaidEvent,
+  OriginatorAdded as OriginatorAddedEvent,
+  RoleAdminChanged as RoleAdminChangedEvent,
+  RoleGranted as RoleGrantedEvent,
+  RoleRevoked as RoleRevokedEvent,
+  Transfer as TransferEvent,
+  Withdraw as WithdrawEvent,
+  YeildDeposited as YeildDepositedEvent
+} from "../generated/Vault/Vault"
+import {
+  Approval,
+  Deposit,
+  Initialized,
+  InvestorAdded,
+  InvestorDeposited,
+  InvestorRedeemed,
+  InvestorWithdrawn,
+  LoanFunded,
+  LoanRepaid,
+  OriginatorAdded,
+  RoleAdminChanged,
+  RoleGranted,
+  RoleRevoked,
+  Transfer,
+  Withdraw,
+  YeildDeposited
+} from "../generated/schema"
+
+export function handleApproval(event: ApprovalEvent): void {
+  let entity = new Approval(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.spender = event.params.spender
+  entity.value = event.params.value
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleDeposit(event: DepositEvent): void {
+  let entity = new Deposit(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.sender = event.params.sender
+  entity.owner = event.params.owner
+  entity.assets = event.params.assets
+  entity.shares = event.params.shares
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleInitialized(event: InitializedEvent): void {
+  let entity = new Initialized(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.version = event.params.version
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleInvestorAdded(event: InvestorAddedEvent): void {
+  let entity = new InvestorAdded(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.vault = event.params.vault
+  entity.user = event.params.user
+  entity.timestamp = event.params.timestamp
+  entity.message = event.params.message
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleInvestorDeposited(event: InvestorDepositedEvent): void {
+  let entity = new InvestorDeposited(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.vault = event.params.vault
+  entity.investor = event.params.investor
+  entity.receiver = event.params.receiver
+  entity.assets = event.params.assets
+  entity.shares = event.params.shares
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleInvestorRedeemed(event: InvestorRedeemedEvent): void {
+  let entity = new InvestorRedeemed(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.vault = event.params.vault
+  entity.investor = event.params.investor
+  entity.receiver = event.params.receiver
+  entity.assets = event.params.assets
+  entity.shares = event.params.shares
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleInvestorWithdrawn(event: InvestorWithdrawnEvent): void {
+  let entity = new InvestorWithdrawn(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.vault = event.params.vault
+  entity.investor = event.params.investor
+  entity.receiver = event.params.receiver
+  entity.assets = event.params.assets
+  entity.shares = event.params.shares
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleLoanFunded(event: LoanFundedEvent): void {
+  let entity = new LoanFunded(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.vault = event.params.vault
+  entity.originator = event.params.originator
+  entity.borrower = event.params.borrower
+  entity.assets = event.params.assets
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleLoanRepaid(event: LoanRepaidEvent): void {
+  let entity = new LoanRepaid(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.vault = event.params.vault
+  entity.originator = event.params.originator
+  entity.borrower = event.params.borrower
+  entity.assets = event.params.assets
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOriginatorAdded(event: OriginatorAddedEvent): void {
+  let entity = new OriginatorAdded(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.vault = event.params.vault
+  entity.user = event.params.user
+  entity.timestamp = event.params.timestamp
+  entity.message = event.params.message
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRoleAdminChanged(event: RoleAdminChangedEvent): void {
+  let entity = new RoleAdminChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.role = event.params.role
+  entity.previousAdminRole = event.params.previousAdminRole
+  entity.newAdminRole = event.params.newAdminRole
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRoleGranted(event: RoleGrantedEvent): void {
+  let entity = new RoleGranted(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.role = event.params.role
+  entity.account = event.params.account
+  entity.sender = event.params.sender
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRoleRevoked(event: RoleRevokedEvent): void {
+  let entity = new RoleRevoked(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.role = event.params.role
+  entity.account = event.params.account
+  entity.sender = event.params.sender
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.value = event.params.value
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleWithdraw(event: WithdrawEvent): void {
+  let entity = new Withdraw(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.sender = event.params.sender
+  entity.receiver = event.params.receiver
+  entity.owner = event.params.owner
+  entity.assets = event.params.assets
+  entity.shares = event.params.shares
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleYeildDeposited(event: YeildDepositedEvent): void {
+  let entity = new YeildDeposited(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.vault = event.params.vault
+  entity.originator = event.params.originator
+  entity.fromAddress = event.params.fromAddress
+  entity.assets = event.params.assets
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
